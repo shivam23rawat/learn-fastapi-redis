20931b6592c6897584b6ac8daa6150aa3435a3b8
Shivam Rawat <shivamrawat2000@gmail.com>
2025-03-14 23:35:24 +0530

    feat(users): ✨ create service class and api for user related operations

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -4,20 +4,21 @@ It imports the necessary modules and includes the sample router with a specified
 Attributes
 ----------
 router : fastapi.APIRouter
-    The main router for version 1 of the API, which includes the sample router.
+    The main router for version 1 of the API.
 Modules
 -------
 fastapi
     The FastAPI framework used to create the API router.
-src.api.v1.sample
-    The module containing the sample router to be included in the main router.
+src.api.v1.users
+    The module containing the user router to be included in the main router.
 """
 
 from fastapi import APIRouter
 
-from src.api.v1.sample import router as sample_router
+from src.api.v1.users import router as user_router
 
 router = APIRouter()
 
-# NOTE: duplicate APIs may show up in swagger but atleast the APIs get clubbed under the same version
-router.include_router(sample_router, prefix="/v1", tags=["v1"])
+# NOTE: duplicate APIs may show up in swagger but atleast the APIs get clubbed
+# under the same version
+router.include_router(user_router, prefix="/v1", tags=["v1"])
